d28c8aafae0ec29685f3f54d4d48319114cd67de
codewithsingh <singhsonu6200@gmail.com>
2025-03-12 14:54:58 +0530

    Updated features or fixed bugs

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = true;
 const server = IS_PROD ?
-    "https://apnavideocallbackend-qvrw.onrender.com" :
+    "https://apnavideocallbackend-mm33.onrender.com/" :
 
     "http://localhost:8000"
 
